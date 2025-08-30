d5e2dca7d373bd9a653f4ff855dfa37c0804ed47
Joshua <st10296167@vcconnect.edu.za>
2025-08-30 20:38:58 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10296167_PROG7312_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
